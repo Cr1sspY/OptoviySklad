7c8c1116dc910af8c30015649dfcfd9ab3f7e881
Cr1sspY <sentluiz14@gmail.com>
2020-10-02 08:35:26 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OptoviySklad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+OptoviySklad Project
